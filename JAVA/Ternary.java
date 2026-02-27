dc845e7b419f48cf217d6c0061f6715f0ce78b9f
Ankit-chaubey1 <chaubeyankit927@gmail.com>
2026-02-28 00:01:38 +0530

    implemented logic of ternary operator

---
+++ JAVA/Ternary.java
@@ -0,0 +1,10 @@
+public class Ternary {
+    public static void main(String[] args) {
+        int x=18;
+    //    String res= (x>18)?"vote":"cant vote";
+    //    System.out.println(res);
+
+       //second way in oneline
+       System.out.println((x>=18)?"vote":"cant vote");
+    }
+}
