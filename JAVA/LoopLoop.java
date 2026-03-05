8751a864134a5b728aedb1d0bb946b5c00d1f5ea
Ankit-chaubey1 <chaubeyankit927@gmail.com>
2026-03-05 15:44:21 +0530

    ok

---
+++ JAVA/LoopLoop.java
@@ -0,0 +1,18 @@
+public class LoopLoop{
+    public static void main(String[]args){
+        //while loop
+        
+        // int countdown=3;
+        // while(countdown>0){
+        //     System.out.println(countdown);
+        //     countdown--;
+        // }
+
+        //do while loop
+        // int i=5;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // }while(i<5);
+    }
+}
